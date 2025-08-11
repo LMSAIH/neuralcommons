13bc23859b9e0006752e56c64cfe3fedbb9d20b5
Axel <yoloax11@gmail.com>
2025-08-10 21:58:27 -0700

    Created home and gallery simple starters to get used to tauri

---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+    <div> 
+        <h1> Neural Commons</h1>
+        <Link to="/import"> Go to Import Gallery</Link>
+    </div>
+    )
+}
+
+
+export default Home;
---
+++ src/views/ImportGallery.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const ImportGallery = () => {
+
+    return (
+    <div> 
+        <h1> Import Gallery</h1>
+        <Link to="/"> Go back home</Link>
+    </div>
+    )
+}
+
+export default ImportGallery;
