accb73c6b294bd5040e8e74984d44106605ce2ea
Axel <yoloax11@gmail.com>
2025-08-29 16:23:11 -0700

    modified the lib to account for fs operations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+use glob::glob;
+use std::path::Path;
+use walkdir::WalkDir;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,14 +10,23 @@ fn greet(name: &str) -> String {
 
 #[tauri::command]
 fn say_something() -> String {
-
     println!("Writing at execution of callable");
     return format!("I am saying something from Rust!");
+}
+
+#[tauri::command]
+fn find_image_paths() -> Vec<String> {
+    vec![
+        String::from("/path/to/image1.png"),
+        String::from("/path/to/image2.jpg"),
+        String::from("/path/to/image3.bmp"),
+    ]
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, say_something])
         .run(tauri::generate_context!())
