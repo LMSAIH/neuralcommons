27136b9a521c8cc52770190efbd83edc35ee2f28
Axel <yoloax11@gmail.com>
2025-08-29 16:22:40 -0700

    Homepage file selection

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
-
+import { invoke } from "@tauri-apps/api/core";
+import FileSelection from "../components/FileSelection";
 const Home = () => {
+    async function sayHi() {
+        alert(await invoke("say_something"));
+    }
     return (
     <div> 
         <h1> Neural Commons</h1>
         <Link to="/import"> Go to Import Gallery</Link>
+        <FileSelection />
     </div>
     )
 }
 
 
 export default Home;
+
